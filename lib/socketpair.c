efe91b050d430844ab73ece678f261b7a56d873b
Dong Kyun Yoo <sokdak@ajou.ac.kr>
2018-11-04 03:34:23 +0900

    [Cleanup] source code

--- lib/socketpair.c
+++ lib/socketpair.c
@@ -30,26 +30,18 @@ int* create_unix_socketpair()
       int join_status_cli = 0;
 
       join_status_cli = pthread_join(pthread_cli, &retval_cli);
-      //printf("retvalcli %d\n", *((int*)retval_cli));
-
       join_status_srv = pthread_join(pthread_srv, &retval_srv);
-      //printf("retvalsrv %d\n", *((int*)retval_srv));
 
       if (join_status_cli || join_status_srv)
         fprintf(stderr, "error occured while join client and server thread\n");
       else
       {
-        //printf("threadstatus; c%d s%d\n", join_status_cli, join_status_srv);
-        //printf("retval s%d c%d\n", *((int*)retval_srv), *((int*)retval_cli));
-
         retpair = (int *)calloc(3, sizeof(int));
         memset(retpair, -1, sizeof(int));
 
         retpair[0] = *((int*)retval_srv) >> 16;
         retpair[1] = *((int*)retval_srv) & 0xFFFF;
         retpair[2] = *((int*)retval_cli);
-
-        //printf("[main] received %d %d %d\n", retpair[0], retpair[1], retpair[2]);
 
         sock_count++; // not to make duplicated socket fd
       }
@@ -115,8 +107,6 @@ void* create_server_unix_socketpair()
   }
   else perror("[s] create socket failed");
 
-  //fprintf(stdout, "res sfd%d cfd%d return%d\n", s_socket_fd, c_socket_fd, success ? ((s_socket_fd << 16) | c_socket_fd) : -1);
-
   if (success)
     retval = (s_socket_fd << 16) | c_socket_fd;
   else retval = -1;
@@ -155,8 +145,6 @@ void* create_client_unix_socketpair()
     usleep(10); // server thread should create socket in 10*CLIENT_SOCK_MAX_RETRIES milliseconds (def: 100ms)
   }
 
-  //fprintf(stdout, "[c]returning cfd%d\n", success ? c_socket_fd : -1);
-  
   if (success)
     retval = c_socket_fd;
   else retval = -1;
